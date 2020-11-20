be63c855d1c9ec72d6796f5311c7ae6694e19209
Marcos Méndez Quintero <38361760+marcos-mendez@users.noreply.github.com>
2020-11-19 23:52:55 -0300

    Tabela já renomeada na v12
    Removi estas linhas para corrigir a Issue https://github.com/OCA/l10n-brazil/issues/1032
    
    Aparentemente isto só deve ser executado quando atualizando todo o Odoo e não quando atualizado o módulo, por algum motivo que desconheço isto esta sendo executado quando atualiza o modulo

--- l10n_br_fiscal/migrations/12.0.3.0.0/pre-migration.py
+++ l10n_br_fiscal/migrations/12.0.3.0.0/pre-migration.py
@@ -3,15 +3,6 @@
 
 from openupgradelib import openupgrade
 
-_column_renames = {
-    'l10n_br_fiscal_document': [
-        ('operation_type', 'fiscal_operation_type')],
-    'l10n_br_fiscal_operation': [
-        ('operation_type', 'fiscal_operation_type')],
-    'l10n_br_fiscal_operation_line': [
-        ('operation_type', 'fiscal_operation_type')],
-}
-
 
 @openupgrade.migrate(use_env=True)
 def migrate(env, version):
